606ff1a66b3086540b2c4a2d649e65959a1ae025
Håkon Åmdal <hakon.amdal@schibsted.com>
2018-04-25 23:24:45 +0200

    More progress

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import _ from 'lodash';
-console.log(_.join(['hehe', 'hoho'],', '));
+import './style.css';
+import pentagon_big from './pentagon-big.svg';
+import polygons from './polygons.json'
+import printMe from './print.js';
+
+function component() {
+    var element = document.createElement('div');
+
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var img = new Image();
+    img.src = pentagon_big;
+
+    element.appendChild(img);
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+
+
+    return element;
+}
+
+document.body.appendChild(component());
+console.log(polygons);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Aspartam Spacemaker Frontend'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
     }
   };
